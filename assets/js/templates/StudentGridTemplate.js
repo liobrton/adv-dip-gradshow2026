1c7336fe613a7c9e81a2db08463efeaa6a2f5516
liobrton <161671530+liobrton@users.noreply.github.com>
2026-09-02 10:35:58 +0800

    feat: template for main page student grid

---
+++ assets/js/templates/StudentGridTemplate.js
@@ -0,0 +1,11 @@
+export function StudentTemplate(data, id) {
+    return `
+          <a href="./student.html?id=${id}" class="max-w-45">
+            <div class="flex flex-col">
+              <img src="assets/images/${id}/profile.png" class="rounded-full" />
+              <h3 class="text-center">${data.name}</h3>
+              <h4 class="text-center">${data.speciality}</h4>
+            </div>
+          </a>
+    `;
+}
