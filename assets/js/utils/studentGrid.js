d8f14816a84238df30f76bf1459cfe6486e4a9d9
liobrton <161671530+liobrton@users.noreply.github.com>
2026-09-09 11:47:33 +0800

    students.json is an object not array

--- assets/js/utils/studentGrid.js
+++ assets/js/utils/studentGrid.js
@@ -1,28 +1,28 @@
 import { StudentGridTemplate } from "../templates/StudentGridTemplate.js";
 
 async function getStudentsData() {
-  const response = await fetch(`/assets/data/students.json`);
-  const students = await response.json();
-  return students;
+    const response = await fetch(`/assets/data/students.json`);
+    const students = await response.json();
+    return students;
 }
 
 export async function buildStudentGrid() {
-  const studentGrid = document.getElementById("studentGrid");
-  const studentsData = await getStudentsData();
-  let studentGridContent = "";
-  
-  // iterate through student data
-  for (let id = 0; id < studentsData.length; id++) {
-    // for every student, run StudentGridTemplate()
-    const student = studentsData[id];
-    console.log(student.name + "'s ID is " + id)
+    const studentGrid = document.getElementById("studentGrid");
+    const studentsData = await getStudentsData();
+    let studentGridContent = "";
 
-    // and append them to the studentGrid element 
-    const studentProfile = StudentGridTemplate(student, id);
-    studentGridContent = studentGridContent + studentProfile;
-  }
+    // iterate through student data
+    for (let id = 0; id < Object.keys(studentsData).length; id++) {
+        // for every student, run StudentGridTemplate()
+        const student = studentsData[id];
+        console.log(student.name + "'s ID is " + id);
 
-  studentGrid.innerHTML = studentGridContent;
+        // and append them to the studentGrid element
+        const studentProfile = StudentGridTemplate(student, id);
+        studentGridContent = studentGridContent + studentProfile;
+    }
+
+    studentGrid.innerHTML = studentGridContent;
 }
 
 document.addEventListener("DOMContentLoaded", async () => {
